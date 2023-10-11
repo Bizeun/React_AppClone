e90bd8419b7a4959783d8393eb9a9b1e6f494101
Bizeun <68911658+Bizeun@users.noreply.github.com>
2023-10-11 00:25:48 -0700

    To test optional props
    by using Typescript and Css syntax

--- react_appclone/src/Circle.tsx
+++ react_appclone/src/Circle.tsx
@@ -1,18 +1,29 @@
 import styled from "styled-components";
 
-interface CircleProps {
-  bgColor: string;
+interface ContainerProps {
+  bgColor: string; //required
+  borderColor: string; //required
 }
 
-const Container = styled.div<CircleProps>`
+interface CircleProps {
+  bgColor: string; //required
+  borderColor?: string; //optional
+  text?: string;
+}
+
+const Container = styled.div<ContainerProps>`
   width: 200px;
   height: 200px;
   background-color: ${(props) => props.bgColor};
   border-radius: 100px;
 `;
 
-function Circle({ bgColor }: CircleProps) {
-  return <Container bgColor={bgColor} />;
+function Circle({ bgColor, borderColor, text = "default" }: CircleProps) {
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
+    </Container>
+  ); //default bordercolor is bgColor
 }
 
 export default Circle;
